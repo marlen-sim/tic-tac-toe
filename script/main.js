3c897c883e0ee9e514cf2b419f5feb2f1d825da0
marlen-sim <simonyan.marlen@gmail.com>
2024-01-23 21:25:09 +0300

    Add winner check loop

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-function gameBoard() {
+const gameBoard = (function () {
     const board = {
         game: [
             ["", "", ""],
@@ -8,8 +8,7 @@ function gameBoard() {
     };
     const getBoard = () => board;
     return { board, getBoard };
-}
-gameBoard();
+})();
 
 function createPlayer(name, marker) {
     const playerName = name;
@@ -24,18 +23,16 @@ function createPlayer(name, marker) {
     return { playerName, playerMarker, giveScore, getScore };
 }
 
-function addToken() {
-    let value = "";
-    const addMarker = (playerMarker) => {
-        value = playerMarker;
-    };
-
-    return { addMarker };
-}
 const playerOne = createPlayer("Marlen", "X");
 const playerTwo = createPlayer("Computer", "O");
 
+console.log(playerOne.playerName);
+
 function gameFlow(playerOne = "player one", playerTwo = "player two") {
+    let board = gameBoard.getBoard();
+
+    console.log("BOARD:", board);
+
     const players = [
         {
             name: playerOne.playerName,
@@ -46,15 +43,84 @@ function gameFlow(playerOne = "player one", playerTwo = "player two") {
             marker: playerTwo.playerMarker,
         },
     ];
+
+    // Смена игрока
     let activePlayer = players[0];
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
+
+    function playRound(activePlayer, row, column) {
+        return (board.game[row][column] = activePlayer.marker);
+    }
+
+    playRound(activePlayer, 0, 2);
+    switchPlayerTurn();
+    playRound(activePlayer, 0, 0);
+    playRound(activePlayer, 1, 1);
+    playRound(activePlayer, 2, 2);
+
+    // Проверка победителя
+    for (i = 0; i < 3; i++) {
+        if (
+            (board.game[i][0] &&
+                board.game[i][1] &&
+                board.game[i][2] === activePlayer.marker) ||
+            (board.game[0][i] &&
+                board.game[1][i] &&
+                board.game[2][i] === activePlayer.marker) ||
+            (board.game[0][0] &&
+                board.game[1][1] &&
+                board.game[2][2] === activePlayer.marker) ||
+            (board.game[0][2] &&
+                board.game[1][1] &&
+                board.game[2][0] === activePlayer.marker)
+        ) {
+            console.log(`${activePlayer.name} wins`);
+            break;
+        } else {
+            return;
+        }
+    }
+
+    /*
+    Вариант проверки через свич
+
+    for (let i = 0; i < 3; i++) {
+        switch (true) {
+            case board.game[i][0] &&
+                board.game[i][1] &&
+                board.game[i][2] === "O":
+                console.log("Player two win");
+                break;
+            case board.game[0][i] &&
+                board.game[1][i] &&
+                board.game[2][i] === "O":
+                console.log("Player two win");
+                break;
+            case board.game[0][0] &&
+                board.game[1][1] &&
+                board.game[2][2] === "O":
+                console.log("Player two win");
+                break;
+
+            case board.game[0][2] &&
+                board.game[1][1] &&
+                board.game[2][0] === "O":
+                console.log("Player two win");
+                break;
+            default:
+                return console.log("tie");
+        }
+    }
+    */
 }
 
+console.log(gameBoard.boardObj);
 playerOne.giveScore();
 let a = playerOne.getScore();
 
 gameFlow(playerOne, playerTwo);
 console.log(a);
-// console.log(borad);
+
+console.log(gameBoard.getBoard());
