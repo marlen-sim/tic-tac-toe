78303e455c4d824c8cc59d357af89cb6b5c13b93
marlen-sim <simonyan.marlen@gmail.com>
2024-09-06 14:26:48 +0300

    Add checking of the spot

--- script/main.js
+++ script/main.js
@@ -8,13 +8,13 @@ const gameBoard = {
 
 const players = {
   playerOne: {
-    name: 'first',
+    name: 'First',
     marker: 'x',
   },
 
   playerTwo: {
     name: 'second',
-    marker: 'y',
+    marker: 'o',
   },
 };
 
@@ -22,6 +22,12 @@ let currentMarker = players.playerOne.marker;
 
 const game = {
   addMarkerToBoard(row, column, currentMarker) {
+    if (
+      gameBoard.board[row][column] === 'x' ||
+      gameBoard.board[row][column] === 'o'
+    ) {
+      return console.log( 'This spot is taken!')
+    }
     gameBoard.board[row][column] = currentMarker;
   },
   switchPlayer(plyerMaker) {
@@ -49,18 +55,24 @@ const game = {
         gameBoard.board[1][1] === currentMarker &&
         gameBoard.board[2][0] === currentMarker)
     ) {
-      return console.log(`${players.playerOne.name} win`);
+      return console.log(`${players.playerOne.name} won the game`);
     } else {
       console.log('play again');
     }
-  },
+  }, 
+  } 
 };
 
 game.addMarkerToBoard(0, 0, currentMarker);
+game.switchPlayer(currentMarker);
+game.addMarkerToBoard(0, 0, currentMarker);
 game.checkWinner();
 game.addMarkerToBoard(1, 1, currentMarker);
+game.switchPlayer(currentMarker);
 game.checkWinner();
 game.addMarkerToBoard(2, 2, currentMarker);
+game.switchPlayer(currentMarker);
 game.checkWinner();
+
 
 console.log(gameBoard.board);
