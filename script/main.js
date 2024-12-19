9c6227366fd6056224d7ef5a5b3ad569f72f7ed6
marlen-sim <simonyan.marlen@gmail.com>
2024-12-19 19:51:00 +0300

    Create working console version

--- script/main.js
+++ script/main.js
@@ -1,92 +1,108 @@
 const gameboard = (function () {
   const board = [
-    [[], [], []],
-    [[], [], []],
-    [[], [], []],
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
   ];
 
   const getBoard = () => board;
-
   return { getBoard };
 })();
-
 
 function createPlayer(name, marker) {
   const playerName = name;
   const playerMarker = marker;
   return { playerName, playerMarker };
 }
-console.log(createPlayer('marlen', 'x'));
 
-const players = {
-  playerOne: {
-    name: 'First',
-    marker: 'x',
-  },
+function gameController() {
+  const playerOne = createPlayer('marlen', 'x');
+  const playerTwo = createPlayer('Ai', 'o');
+  let activePLayer = playerOne;
+  let currentMarker = playerOne.playerMarker;
 
-  playerTwo: {
-    name: 'second',
-    marker: 'o',
-  },
-};
+  const getActivePlayer = () => activePLayer;
 
-let currentMarker = players.playerOne.marker;
+  const board = gameboard.getBoard();
 
-// const game = {
-//   addMarkerToBoard(row, column, currentMarker) {
-//     if (
-//       gameBoard.board[row][column] === 'x' ||
-//       gameBoard.board[row][column] === 'o'
-//     ) {
-//       return console.log('This spot is taken!');
-//     }
-//     gameBoard.board[row][column] = currentMarker;
-//   },
+  const switchPlayer = () => {
+    if (currentMarker === 'x') {
+      currentMarker = playerTwo.playerMarker;
+      activePLayer = playerTwo;
+    } else {
+      currentMarker = playerOne.playerMarker;
+      activePLayer = playerOne;
+    }
+    return currentMarker;
+  };
 
-//   switchPlayer(plyerMaker) {
-//     if (plyerMaker === 'x') {
-//       currentMarker = players.playerOne.marker;
-//     } else {
-//       currentMarker = players.playerTwo.marker;
-//     }
-//   },
+  const checkEmptyBoard = () => {
+    let isFull = board.flat().every((cell) => cell === 'x' || cell === 'o');
+    if (isFull) console.log('The cell is full');
+    return isFull;
+  };
 
-//   checkWinner() {
-//     if (game.checkEmptyBoard()) {
-//       return console.log('The board is full! Tie!');
-//     }
+  const addMarkerToBoard = (row, column, currentMarker) => {
+    if (board[row][column] === 'x' || board[row][column] === 'o') {
+      return console.log('This spot is taken!');
+    }
+    board[row][column] = currentMarker;
+  };
 
-//     let isRowWinner =
-//       (gameBoard.board[0][0] === currentMarker &&
-//         gameBoard.board[0][1] === currentMarker &&
-//         gameBoard.board[0][2] === currentMarker) ||
-//       (gameBoard.board[1][0] === currentMarker &&
-//         gameBoard.board[1][1] === currentMarker &&
-//         gameBoard.board[1][2] === currentMarker) ||
-//       (gameBoard.board[2][0] === currentMarker &&
-//         gameBoard.board[2][1] === currentMarker &&
-//         gameBoard.board[2][2] === currentMarker);
+  const checkWinner = () => {
+    let isRowWinner =
+      (board[0][0] === currentMarker &&
+        board[0][1] === currentMarker &&
+        board[0][2] === currentMarker) ||
+      (board[1][0] === currentMarker &&
+        board[1][1] === currentMarker &&
+        board[1][2] === currentMarker) ||
+      (board[2][0] === currentMarker &&
+        board[2][1] === currentMarker &&
+        board[2][2] === currentMarker);
 
-//     let isDiagonalWinner =
-//       (gameBoard.board[0][0] === currentMarker &&
-//         gameBoard.board[1][1] === currentMarker &&
-//         gameBoard.board[2][2] === currentMarker) ||
-//       (gameBoard.board[0][2] === currentMarker &&
-//         gameBoard.board[1][1] === currentMarker &&
-//         gameBoard.board[2][0] === currentMarker);
+    let isColumnWinner =
+      (board[0][0] === currentMarker &&
+        board[1][0] === currentMarker &&
+        board[2][0] === currentMarker) ||
+      (board[0][1] === currentMarker &&
+        board[1][1] === currentMarker &&
+        board[2][1] === currentMarker) ||
+      (board[0][2] === currentMarker &&
+        board[1][2] === currentMarker &&
+        board[2][2] === currentMarker);
 
-//     if (isRowWinner || isDiagonalWinner) {
-//       return console.log(`${currentMarker} won the game`);
-//     } else {
-//       console.log(`${currentMarker} turn`);
-//     }
-//   },
+    let isDiagonalWinner =
+      (board[0][0] === currentMarker &&
+        board[1][1] === currentMarker &&
+        board[2][2] === currentMarker) ||
+      (board[0][2] === currentMarker &&
+        board[1][1] === currentMarker &&
+        board[2][0] === currentMarker);
 
-//   checkEmptyBoard() {
-//     let isFull = gameBoard.board
-//       .flat()
-//       .every((cell) => cell === 'x' || cell === 'o');
+    if (isRowWinner || isDiagonalWinner || isColumnWinner) {
+      console.log(`${getActivePlayer().playerName} won the game`);
+      return;
+    } else {
+      console.log(`${getActivePlayer().playerName}'s turn`);
+    }
+  };
 
-//     return isFull;
-//   },
-// };
+  const playRound = (row, column) => {
+    checkEmptyBoard();
+    addMarkerToBoard(row, column, currentMarker);
+    checkWinner();
+    switchPlayer();
+  };
+
+  playRound(0, 1);
+  playRound(0, 2);
+  playRound(2, 1);
+  playRound(2, 0);
+  playRound(2, 2);
+  playRound(1, 1);
+
+  console.log(board);
+}
+
+gameController();
